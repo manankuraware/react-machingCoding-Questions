2b945f2f32d31c87169ba15e2d9e393f06f16d46
manankuraware <manankuraware2017@gmail.com>
2024-01-30 15:37:53 +0530

    first commit

--- cartSlider/src/App.jsx
+++ cartSlider/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-3/4 m-auto">
+      <div className="mt-20">
+        <Slider {...settings}>
+          {data.map((d) => (
+            <div
+              key={d.name}
+              className="bg-white h-[450px] text-black rounded-xl"
+            >
+              <div className="h-56 bg-indigo-500 flex justify-center items-center rounded-t-xl">
+                <img src={d.img} alt="" className="h-44 w-44 rounded-full" />
+              </div>
+
+              <div className="flex flex-col items-center justify-center gap-4 p-4">
+                <p className="text-xl font-semibold">{d.name}</p>
+                <p className="text-center">{d.review}</p>
+                <button className="bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl">
+                  Read More
+                </button>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+const data = [
+  {
+    name: `John Morgan`,
+    img: `/students/John_Morgan.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Ellie Anderson`,
+    img: `/students/Ellie_Anderson.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Nia Adebayo`,
+    img: `/students/Nia_Adebayo.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Rigo Louie`,
+    img: `/students/Rigo_Louie.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Mia Williams`,
+    img: `/students/Mia_Williams.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+];
+
+export default App;
